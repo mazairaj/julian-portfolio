6783f6ad2f1d477bd79d596eaea7372f62230ef2
Julian Mazaira <jmazaira@smarterd.com>
2024-08-14 15:29:28 -0400

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? '/julian-portfolio' : '',
+    assetPrefix: isProd ? '/julian-portfolio/' : '',
     images: {
         remotePatterns: [
           {
