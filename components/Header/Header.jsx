40430554c2079ae8e83e489014a794d4650cfb31
Julian Mazaira <jmazaira@smarterd.com>
2024-08-22 14:17:55 -0400

    images

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -11,19 +11,35 @@ export default function Header() {
         <header className="py-8 xl:py-12 text-white">
             <div className="container mx-auto flex justify-between items-center">
                 {/* Logo */}
-                <Link href="/">
+                    {/* <img 
+                    src="/assets/logos/JulianBuilds_40x40px.svg" 
+                    alt="Julian Builds" 
+                    style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+                    /> */}
+                {/* <Link href="/">
+                <div style={{ position: 'relative', width: '100px', height: '100px' }}>
+                    <Image 
+                        src="/assets/logos/jb_transparent_green.png"
+                        alt="Julian Builds"
+                        height="100"
+                        width="100"
+                        // objectFit="contain" // Removed this as it's being applied in the style prop
+                        style={{ objectFit: 'contain' }} // Use the appropriate objectFit value
+                    />
+                </div>
+                </Link> */}
+                <Link href="/" className="flex gap-6">
+                    <Image 
+                        src="/assets/logos/julianbuilds_225x225px.png"
+                        alt="JB"
+                        height="40"
+                        width="40"
+                        // objectFit="contain" // Removed this as it's being applied in the style prop
+                        style={{ objectFit: 'contain' }} // Use the appropriate objectFit value
+                    />
                     <h1 className="text-4xl font-semibold">
                         Julian <span className="text-accent">.</span>
                     </h1>
-                        {/* <div style={{ height: '150px', width: 'auto', position: 'relative' }}>
-                            <Image 
-                            src="/assets/white-signature.svg"
-                            alt="Logo" 
-                            layout="fill" 
-                            objectFit="contain"
-                            />
-                        </div>
-                        <span className="text-accent">.</span> */}
                 </Link>
                 <div className="hidden xl:flex items-center gap-8">
                     <Nav />
